173e1565d7a1f05c24ef22533f50430457130063
Joao Nascimento <joao.nascimento@ifrn.edu.br>
2019-06-07 15:14:42 -0300

    Primeiro commit

---
+++ js/index.js
@@ -0,0 +1,25 @@
+let data = document.querySelector("#data");
+let quantidade = document.querySelector("#quantidade");
+let valor = document.querySelector("#valor");
+
+let campos = [data,quantidade,valor];
+
+let form = document.querySelector("form");
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    let tr = document.createElement("tr");
+    campos.forEach((campo)=>{
+        let td = document.createElement("td");
+        td.textContent = campo.value;        
+        tr.appendChild(td);
+    });
+    let total = campos[1].value * campos[2].value;
+    let td = document.createElement("td");
+    td.textContent = total;
+    tr.appendChild(td);
+
+    let tbody = document.querySelector("table tbody");
+    tbody.appendChild(tr);
+});
+
